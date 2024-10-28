1ae9b029c7062491c43f456c8ffb8a4905ff3440
Leslie Rodriguez <leslie286@usf.edu>
2024-10-27 23:51:27 -0400

    Add event listener for product selection and quantity input

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+//Task 2: Add event listener for Product Selection
+document.addEventListener(' () => {
+    const productSelect = document.getElementById('productSelect');
+    const quantityInput = document.getElementById('quantityInput');
+    const priceDisplay = document.getElementById('priceDisplay');
+
+    function updatePrice() {
+        const selectedOption = productSelect.options[productSelect.selectedIndex];
+        const quantity = parseInt(quantityInput.value, 10);
+    }
+        productSelect.addEventListener('change', updatePrice);
+        quantityInput.addEventListener('input', updatePrice);
+});
